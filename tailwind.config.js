dd6260666110ffa3313a4ad92d755ef6111b7410
Micha <micha-elmans@outlook.com>
2023-07-18 21:39:14 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  mode: "jit",
+  content: ['./resources/**/*.{php,js,html}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
