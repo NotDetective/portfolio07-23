01b6eb2c3a0e99ff3d8373c4719ffd1eb841e11f
Micha <micha-elmans@outlook.com>
2023-07-20 15:11:06 +0200

    header and footer frame done
    header and footer style done
    added custom colors for tailwind

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -4,27 +4,40 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
     <title>Portfolio Micha</title>
 </head>
 <body>
     
-    <header>
-        <div>
-
+    <header class="px-[5%] w-full h-24 drop-shadow-md bg-white flex justify-between items-center">
+        <div class="flex flex-col h-auto justify-center items-center">
+            <h1 class="text-4xl font-inter drop-shadow-md text-lightblue font-bold">マイカ</h1>
+            <div class="h-2 w-40 bg-lightblue"></div>
+            <h1 class="text-4xl font-inter drop-shadow-md text-lightblue font-bold">Micha</h1>
         </div>
-        <nav>
-            <a href="">
-                <h1>About Me</h1>
+        <nav class="flex justify-between w-1/4">
+            <a href="/#">
+                <h1 class="text-4xl font-inter drop-shadow-md text-lightblue font-bold">About Me</h1>
             </a>
-            <a href="">
-                <h1>My Work</h1>
+            <a href="/#">
+                <h1 class="text-4xl font-inter drop-shadow-md text-lightblue font-bold" >My Work</h1>
             </a>
         </nav>
     </header>
 
     @yield('content')
+    
 
-    <footer></footer>
+    <footer>
+        <svg xmlns="http://www.w3.org/2000/svg" height="48" viewBox="0 -960 960 960" width="48" class=" fill-lightblue fixed bottom-3 right-2/4 hover:cursor-pointer">
+            <path d="M480-80 200-360l42-42 208 208v-686h60v686l208-208 42 42L480-80Z"/>
+        </svg>
+        <div class=" h-24 w-24 rounded-full flex justify-center items-center border-4 border-lightblue fill-lightblue fixed bottom-3 right-3 hover:bg-lightblue hover:fill-white hover:cursor-pointer">
+            <svg xmlns="http://www.w3.org/2000/svg" height="48" viewBox="0 -960 960 960" width="48" class="rotate-180">
+                <path d="M480-80 200-360l42-42 208 208v-686h60v686l208-208 42 42L480-80Z"/>
+            </svg>
+        </div>
+    </footer>
 
 </body>
 </html>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,22 @@ export default {
   mode: "jit",
   content: ['./resources/**/*.{php,js,html}'],
   theme: {
-    extend: {},
+    colors: {
+      lightblue: '#0FA3E2',
+      darkblue: '#086788',
+      lighterblue: '#189AB4',
+
+      white: '#ffffff',
+
+      red: '#ff0000',
+      green: '#00ff00',
+      blue: '#0000ff'
+    },
+    extend: {
+      fontFamily: {
+        inter: ['Inter var'],
+      }
+    },
   },
   plugins: [],
 }
