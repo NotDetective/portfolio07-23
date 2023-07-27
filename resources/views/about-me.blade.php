de382b068f5a2efd0c52bb24afdf11c0dfb70c33
Micha <micha-elmans@outlook.com>
2023-07-27 23:27:43 +0200

    right site partially done
    right site of about me is partially done needs more details some logic

--- resources/views/about-me.blade.php
+++ resources/views/about-me.blade.php
@@ -20,20 +20,76 @@
                 </div>
             </div>
         </div>
-    
-        <div class="">
-            <h1>Skills</h1>
-        </div>
-        <div>
-            {{-- alls programming lauges --}}
-        </div>
-        <div>
-            <h1></h1>
-            <div>
-                <p></p>
-                <p></p>
+        
+        <div class="h-[775px] w-[750px] bg-white drop-shadow-md flex flex-col items-center justify-between p-7">
+            <div class="w-[700px] h-[56px] border-b-4 border-lightpsecondary flex items-center justify-center">
+                <h1 class="text-4xl font-bold">Skills</h1>
+            </div>
+            <div class="grid grid-cols-2">
+
+                <div class="flex flex-col items-start w-[360px]">
+                    <div class="flex items-center">
+                        <div class="bg-[#E44B23] h-[36px] w-[36px] rounded-full"></div>
+                        <div class="w-[20px]"></div>
+                        <h1 class="text-[35px]">HTML</h1>
+                    </div>
+                    <p>Used to make websites</p>
+                </div>
+
+                <div class="flex flex-col items-start w-[360px]">
+                    <div class="flex items-center">
+                        <div class="bg-[#563D7C] h-[36px] w-[36px] rounded-full"></div>
+                        <div class="w-[20px]"></div>
+                        <h1 class="text-[35px]">CSS</h1>
+                    </div>
+                    <p>Used to style websites</p>
+                </div>
+
+                <div class="flex flex-col items-start w-[360px]">
+                    <div class="flex items-center">
+                        <div class="bg-[#F1E05A] h-[36px] w-[36px] rounded-full"></div>
+                        <div class="w-[20px]"></div>
+                        <h1 class="text-[35px]">JavaScript</h1>
+                    </div>
+                    <p>Used to make websites interactive</p>
+                </div>
+
+                <div class="flex flex-col items-start w-[360px]">
+                    <div class="flex items-center">
+                        <div class="bg-[#B07219] h-[36px] w-[36px] rounded-full"></div>
+                        <div class="w-[20px]"></div>
+                        <h1 class="text-[35px]">Java</h1>
+                    </div>
+                    <p>Used for making applications</p>
+                </div>
+
+                <div class="flex flex-col items-start w-[360px]">
+                    <div class="flex items-center">
+                        <div class="bg-[#4F5D95] h-[36px] w-[36px] rounded-full"></div>
+                        <div class="w-[20px]"></div>
+                        <h1 class="text-[35px]">PHP</h1>
+                    </div>
+                    <p>Used for backend of websites</p>
+                </div>
+
+                <div class="flex flex-col items-start w-[360px]">
+                    <div class="flex items-center">
+                        <div class="bg-[#4F5D95] h-[36px] w-[36px] rounded-full"></div>
+                        <div class="w-[20px]"></div>
+                        <h1 class="text-[35px]">Laravel</h1>
+                    </div>
+                    <p>framework of php</p>
+                </div>
+            </div>
+            <div class="border-t-4 border-lightpsecondary w-[700px] flex flex-col h-[300px] justify-evenly items-center">
+                <h1 class="text-3xl font-bold">What am I doing now?</h1>
+                <div class="flex justify-evenly w-[750px]">
+                    <p class="text-2xl w-[350px]">I am now studying to become a software developer.</p>
+                    <p class="text-2xl w-[350px]">I am currently going to ROC Nijmegen and I am a year one student. </p>
+                </div>
             </div>
         </div>
+        
 
     </section>
 @endsection
