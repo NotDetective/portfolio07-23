68cdcc5449612a4693677a4fc398ea947a20a991
Micha <micha-elmans@outlook.com>
2023-07-30 19:18:50 +0200

    change color
    change color still needs to change the color of the div
    small changes is changeAge

--- public/js/changeAge.js
+++ public/js/changeAge.js
@@ -1,27 +1,19 @@
-const calAge = () => {
-
+const calAge = (birthDay, birthMonth, birthYear) => {
     let ageCal = 0
 
     const date = new Date()
+    let month = date.getMonth() + 1
+    let year = date.getFullYear()
+    let day = date.getDate()
 
-    let month= date.getMonth()+1
+    if (month > 3 || (month == birthMonth && day >= birthDay)) {
+        ageCal = year - birthYear;
+      } else {
+        ageCal = year - birthYear - 1;
+      }
 
-    let year= date.getFullYear()
-
-    let day= date.getDate()
-
-    if(month == 3 && day >= 9){
-
-        ageCal = year - 2006
-
-    }else if (month >= 4) {
-    
-        ageCal = year - 2006
-
-    }else {
-        ageCal = year - 2006 - 1
-    }
-    changeAge(ageCal)
+    console.log(ageCal)
+    // changeAge(ageCal)
 }
 
 const changeAge = (ageInYears) => {
@@ -29,6 +21,6 @@ const changeAge = (ageInYears) => {
     ageElement.innerHTML = ageInYears
 }
 
-calAge()
+calAge(3,9,2006)
 
 
