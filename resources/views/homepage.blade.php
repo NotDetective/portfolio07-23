e493eddb6aad5afa04c1882f0bbd1eac5dd1b9a1
Micha <micha-elmans@outlook.com>
2023-11-30 20:34:13 +0100

    homepage done
    added padding arround
    added code

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -1,47 +1,37 @@
 <x-layout>
 
-    <div class="w-full h-full flex">
-        <section class="h-5/6 w-1/2 flex items-center justify-center">
+    @php
+        $text = file_get_contents(resource_path('views/homepage.blade.php'));
+    @endphp
 
-            <div class="flex flex-col">
-                <h1 class="text-7xl text-gradient">
-                    Hi,
-                    <br>
-                    I'm Micha,
-                    <br>
-                    A software dev,
-                    <br>
-                    From the Netherlands.
-                </h1>
+    <div class="w-full h-full flex p-5">
+        <div class="w-full h-full flex">
+            <section class="h-5/6 w-1/2 flex items-center justify-center">
 
-                <p class="text-4xl text-purpleDark/60">Software Developer student</p>
+                <div class="flex flex-col">
+                    <h1 class="text-7xl text-gradient">
+                        Hi,
+                        <br>
+                        I'm Micha,
+                        <br>
+                        A software dev,
+                        <br>
+                        From the Netherlands.
+                    </h1>
 
-                <div class="mt-6 flex w-3/5 justify-between">
-
-
-                    <p class="text-4xl border-background hover:border-purple border-b-4">
-                        <a href="">
-                            About Me
-                        </a>
-                    </p>
-
-
-                    <p class="text-4xl border-background hover:border-purple border-b-4">
-                        <a href="">
-                            Contact
-                        </a>
-                    </p>
-
-
+                    <p class="text-4xl text-purpleDark/60">Software Developer student</p>
                 </div>
-            </div>
 
-        </section>
+            </section>
 
-        <section class="h-full w-1/2">
-
-        </section>
+            <section class="h-full w-1/2 flex items-center text-right mr-5">
+                <pre class="h-11/12">
+                    <code class="text-2xl text-gradient w-full">
+                        {{ $text }}
+                    </code>
+                </pre>
+            </section>
+        </div>
     </div>
 
 </x-layout>
-
