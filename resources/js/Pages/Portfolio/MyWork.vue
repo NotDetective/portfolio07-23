35e7ed0fb1ac01ca22cca8b83e52a43e671cb32f
Micha <micha-elmans@outlook.com>
2024-01-30 23:45:29 +0100

    made a start to the my work page

---
+++ resources/js/Pages/Portfolio/MyWork.vue
@@ -0,0 +1,49 @@
+<script setup>
+
+</script>
+
+<template>
+
+    <main
+        class="flex "
+    >
+
+        <section>
+            <div class="w-fit h-fit px-5 py-8 bg-white shadow-md rounded-lg flex flex-col items-center gap-5 text-3xl">
+                <h1 class="text-4xl text-center">Github</h1>
+                <div class="border-gradient border-b-4 w-80"/>
+
+                <img
+                    class="rounded-full w-52 h-52"
+                    src="https://placehold.co/300"
+                >
+                <p>
+                    NotDetective
+                </p>
+                <p>
+                    Student ROC Nijmgen
+                </p>
+                <p>
+                    Public Repos: 5
+                </p>
+            </div>
+
+            <div class="flex flex-col gap-5 w-52">
+                <button>
+                    Repositories (5)
+                </button>
+                <button>
+                    Projects Worked On (0)
+                </button>
+            </div>
+        </section>
+
+        <section
+            class=""
+        >
+
+        </section>
+
+    </main>
+
+</template>
